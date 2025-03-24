76c5a96f64b518e1216391825760230dbf623f6e
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-03-24 16:39:45 -0700

    Configured Middleware in the Program class and made a new folder with a "RequestLoggingMiddleware.cs" file inside of it.

--- COMP003B.LectureActivity2/Program.cs
+++ COMP003B.LectureActivity2/Program.cs
@@ -1,36 +1,37 @@
 namespace COMP003B.LectureActivity2
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			// Middleware sequence
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseMiddleware<COMP003B.LectureActivity2.Middleware.RequestLoggingMiddleware>();
+			app.UseWelcomePage("/welcome");
+			app.UseRouting();
+			app.UseAuthorization();
 
-            app.UseRouting();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
